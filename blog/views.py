2edc3c9c84c4daff7c408753941a8024d96e987f
Sandy Says <sandilyakaravadi@friction.com>
2020-03-02 19:23:27 +0530

    added TITLE tag in content and included local_settings also

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,13 @@ from .models import Blog
 # Create your views here.
 # Lists all Blogs
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')
+    blogs = Blog.objects.all()
     # content
     content = {'blogs': blogs}
     return render(request, 'blog/all_blogs.html', context=content)
 
 def detailBlog(request, blog_id):
+    print("Id :- {}".format(blog_id))
     # fetching Blog by Id
     blogItem = get_object_or_404(Blog, pk=blog_id)
     # content
